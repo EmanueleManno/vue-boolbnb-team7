cd31f2ce0ef56d8e9bbcd4a2ccc77ccef6f99faa
EmanueleManno <mannus1994@gmail.com>
2023-09-22 12:52:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<!--Script-->
+<script></script>
+
+<!--Template-->
+<template>
+  <h2 class="text-danger container">Team 7</h2>
+</template>
+
+<!--Style-->
+<style lang="scss">
+@use './assets/scss/style.scss';
+</style>
