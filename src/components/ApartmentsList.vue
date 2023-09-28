062471c8f584be09eabb5e740ab3cca58763dfd8
Riccardo Basso <riccardo.basso2001@gmail.com>
2023-09-28 16:03:53 +0200

    fix size img

--- src/components/ApartmentsList.vue
+++ src/components/ApartmentsList.vue
@@ -19,9 +19,9 @@ export default {
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5">
             <div v-for="apartment in apartments" :key="apartment.id" class="col">
                 <div class="card">
-                    <a href="">
+                    <a href="#">
                         <div class="card-img">
-                            <img :src="apartment.image" class="card-img-top" :alt="apartment.title">
+                            <img :src="apartment.image" :alt="apartment.title">
                         </div>
                         <div class="card-body">
                             <h6 class="card-title">{{ apartment.title }}</h6>
@@ -46,7 +46,44 @@ export default {
 }
 
 .card-img {
-    border-radius: 15px;
+    @include square(calc(100vw - 24px), 15px);
+    max-width: 100%;
     overflow: hidden;
+
+    img {
+        @include square(100%);
+        object-fit: cover;
+    }
+}
+
+// Media Query
+@media (min-width: 576px) {
+    .card-img {
+        max-height: 514px;
+    }
+}
+
+@media (min-width: 768px) {
+    .card-img {
+        max-height: 334px;
+    }
+}
+
+@media (min-width: 992px) {
+    .card-img {
+        max-height: 293px;
+    }
+}
+
+@media (min-width: 1200px) {
+    .card-img {
+        max-height: 259px;
+    }
+}
+
+@media (min-width: 1400px) {
+    .card-img {
+        max-height: 238px;
+    }
 }
 </style>
