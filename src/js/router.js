64d6f4e8ef6b41c27cf18a0f0c69af926b09ee2c
l3on <l3onwebdev@gmail.com>
2023-10-03 17:09:53 +0200

    added page to router

--- src/js/router.js
+++ src/js/router.js
@@ -1,12 +1,14 @@
-import {createRouter, createWebHistory} from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
+import AdvancedSearchPage from '../pages/AdvancedSearchPage.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path:'/', name: 'home', component: HomePage},
-        {path:'/:pathMatch(.*)*', redirect: '/'},
+        { path: '/', name: 'home', component: HomePage },
+        { path: '/advanced-search', name: 'advanced-search', component: AdvancedSearchPage },
+        { path: '/:pathMatch(.*)*', redirect: '/' },
     ]
 });
 
-export{router};
+export { router };
