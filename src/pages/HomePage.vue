77c2e018e988bd4e7ed6ff372b36033e25801522
l3on <l3onwebdev@gmail.com>
2023-10-03 20:23:44 +0200

    changed apartment list component logic

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,30 @@
 <script>
 import AppNavbar from '../components/AppNavbar.vue';
+import AppLoader from '../components/AppLoader.vue';
+
+import axios from 'axios';
+const endpoint = 'http://127.0.0.1:8000/api/apartments'
+
 import ApartmentsList from '../components/ApartmentsList.vue';
 export default {
-    components: { AppNavbar, ApartmentsList },
+    components: { AppNavbar, ApartmentsList, AppLoader },
+    data: () => ({ apartments: [], isLoading: true }),
+    methods: {
+        fetchApartments() {
+            axios.get(endpoint).then(res => { this.apartments = res.data; this.isLoading = false; });
+        }
+    },
+    created() {
+        this.fetchApartments();
+    }
 }
 </script>
 
 <template>
     <main>
         <AppNavbar />
-        <ApartmentsList />
+        <ApartmentsList :apartments="apartments" />
     </main>
+    <!-- Loader -->
+    <AppLoader :is-loading="isLoading" />
 </template>
