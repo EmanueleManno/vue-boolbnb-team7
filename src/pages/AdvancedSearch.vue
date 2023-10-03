c959d2e0aa15ee483ec3d95a14b74c648e27a596
l3on <l3onwebdev@gmail.com>
2023-10-03 17:09:19 +0200

    added base page structure

---
+++ src/pages/AdvancedSearch.vue
@@ -0,0 +1,14 @@
+<script>
+import AppNavbar from '../components/AppNavbar.vue';
+import ApartmentsList from '../components/ApartmentsList.vue';
+export default {
+    components: { AppNavbar, ApartmentsList },
+}
+</script>
+
+<template>
+    <main>
+        <AppNavbar />
+        <ApartmentsList />
+    </main>
+</template>
