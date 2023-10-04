bcda12d081395a20a0938ee43fc4825f2e2f8951
l3on <l3onwebdev@gmail.com>
2023-10-04 13:26:53 +0200

    added search modal base structure

---
+++ src/components/search/SearchModal.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+
+
+}
+</script>
+
+
+<template>
+    <div class="modal fade" id="searchModal" tabindex="-1" aria-hidden="true">
+
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <!-- Header -->
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5">Filtri</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <!-- Body -->
+                <div class="modal-body">
+                    [... FILTRI ...]
+                </div>
+
+                <!-- Actions -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary">Cancella Tutto</button>
+                    <button type="button" class="btn btn-success">Mostra</button>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</template>
+
+
+<style></style>
