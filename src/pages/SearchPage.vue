73a78cf0a54a5ffa136a27c96a3beaa757a31201
l3on <l3onwebdev@gmail.com>
2023-10-04 12:27:03 +0200

    added lat watcher and fetch apartments

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,26 +1,55 @@
 <script>
 import AppNavbar from '../components/AppNavbar.vue';
+import AppLoader from '../components/AppLoader.vue';
 import ApartmentsList from '../components/ApartmentsList.vue';
 
 import axios from 'axios';
 const endpoint = 'http://127.0.0.1:8000/api/apartments/filter';
-import { store } from '../js/store';
 
 
 export default {
-    components: { AppNavbar, ApartmentsList },
-    data: () => ({ apartments: [] }),
+    components: { AppNavbar, ApartmentsList, AppLoader },
+    data: () => ({
+        apartments: [],
+        isLoading: false,
+    }),
+
+    computed: {
+        lat() {
+            return this.$route?.query.lat;
+        },
+        lon() {
+            return this.$route?.query.lon;
+        },
+    },
+
+    watch: {
+        lat: {
+            handler(newLat, oldLat) {
+                if (newLat != oldLat) {
+                    this.fetchApartments();
+                }
+            }
+        }
+    },
 
     methods: {
         fetchApartments() {
             const params = {
-                lat: store.lat,
-                lon: store.lon,
+                lat: this.lat,
+                lon: this.lon
             };
+
+            console.log(params);
 
             axios.get(endpoint, { params })
                 .then(res => {
                     this.apartments = res.data;
+                })
+                .catch(err => {
+                    console.error(err.response.data);
+                })
+                .then(() => {
                     this.isLoading = false;
                 });
         }
@@ -36,4 +65,6 @@ export default {
         <AppNavbar />
         <ApartmentsList :apartments="apartments" />
     </main>
+    <!-- Loader -->
+    <AppLoader :is-loading="isLoading" />
 </template>
