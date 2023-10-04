4eece04eb78b27befd6b45403c0c5b9503375f77
l3on <l3onwebdev@gmail.com>
2023-10-04 15:06:47 +0200

    added all filters inputs

--- src/components/search/SearchModal.vue
+++ src/components/search/SearchModal.vue
@@ -1,7 +1,24 @@
 <script>
+const services = [
+    { id: 1, 'name': 'Wi-Fi', 'icon': 'wifi' },
+    { id: 2, 'name': 'TV', 'icon': 'tv' },
+    { id: 3, 'name': 'Lavatrice', 'icon': 'soap' },
+    { id: 4, 'name': 'Asciugatrice', 'icon': 'temperature-high' },
+    { id: 5, 'name': 'Ferro da stiro', 'icon': 'shirt' },
+    { id: 6, 'name': 'Asciugacapelli', 'icon': 'tornado' },
+    { id: 7, 'name': 'Cucina', 'icon': 'kitchen-set' },
+    { id: 8, 'name': 'Aria Condizionata', 'icon': 'temperature-arrow-down' },
+    { id: 9, 'name': 'Riscaldamento', 'icon': 'temperature-arrow-up' },
+    { id: 10, 'name': 'Posto Macchina', 'icon': 'car' },
+    { id: 11, 'name': 'Piscina', 'icon': 'water-ladder' },
+    { id: 12, 'name': 'Portineria', 'icon': 'bell-concierge' },
+    { id: 13, 'name': 'Sauna', 'icon': 'house-tsunami' },
+    { id: 14, 'name': 'Vista Mare', 'icon': 'water' },
+]
 export default {
-
-
+    data: () => ({
+        services
+    })
 }
 </script>
 
@@ -9,7 +26,7 @@ export default {
 <template>
     <div class="modal fade" id="searchModal" tabindex="-1" aria-hidden="true">
 
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
 
                 <!-- Header -->
@@ -20,7 +37,43 @@ export default {
 
                 <!-- Body -->
                 <div class="modal-body">
-                    [... FILTRI ...]
+
+                    <!-- Filters -->
+                    <form @submit.prevent class="container-fluid">
+                        <div class="row">
+
+                            <!-- Rooms -->
+                            <div class="col-12 col-sm-4 mb-3">
+                                <label for="rooms" class="form-label fw-bold">Numero di stanze</label>
+                                <input type="number" class="form-control" id="rooms">
+                            </div>
+
+                            <!-- Beds -->
+                            <div class="col-12 col-sm-4 mb-3">
+                                <label for="beds" class="form-label fw-bold">Numero di letti</label>
+                                <input type="number" class="form-control" id="beds">
+                            </div>
+
+                            <!-- Radius -->
+                            <div class="col-12 col-sm-4 mb-3">
+                                <label for="radius" class="form-label fw-bold">Raggio ricerca [km]</label>
+                                <input type="number" class="form-control" id="radius" value="20">
+                            </div>
+
+                            <!-- Services -->
+                            <div class="col-12 mb-3">
+                                <p class="fw-bold mb-2">Servizi</p>
+                                <div class="row">
+                                    <div v-for="service in services" :key="service.id" class="col-6 form-check">
+                                        <input class="form-check-input" type="checkbox" :id="service.id">
+                                        <label class="form-label d-block" :for="service.id">{{ service.name
+                                        }}</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </form>
                 </div>
 
                 <!-- Actions -->
