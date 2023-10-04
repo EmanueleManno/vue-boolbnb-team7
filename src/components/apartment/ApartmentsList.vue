91ae35f61c47dad018f2cc94a68b30d434f411a0
l3on <l3onwebdev@gmail.com>
2023-10-04 18:13:18 +0200

    added empty apartments message

--- src/components/apartment/ApartmentsList.vue
+++ src/components/apartment/ApartmentsList.vue
@@ -12,12 +12,13 @@ export default {
 
 <template>
     <div class="container">
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5">
+        <div v-if="apartments.length" class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5">
             <div v-for="apartment in apartments" :key="apartment.id" class="col">
                 <!--Card-->
                 <ApartmentCard :apartment="apartment" />
             </div>
         </div>
+        <h4 v-else class="text-center mt-5">Non ci sono appartamenti.</h4>
     </div>
 </template>
 
